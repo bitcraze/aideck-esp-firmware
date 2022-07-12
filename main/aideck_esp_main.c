9a10790765c479e46eb5578db83b21b2b5d25ac0
Marcus <marcus.c.eliasson@gmail.com>
2022-07-12 13:04:36 +0200

    Increase the CPX baudrate to 576000

--- main/aideck_esp_main.c
+++ main/aideck_esp_main.c
@@ -153,7 +153,7 @@ void app_main(void)
     spi_transport_init();
 
     const uart_config_t uart_config = {
-      .baud_rate = 115200,
+      .baud_rate = 576000,
       .data_bits = UART_DATA_8_BITS,
       .parity = UART_PARITY_DISABLE,
       .stop_bits = UART_STOP_BITS_1,
